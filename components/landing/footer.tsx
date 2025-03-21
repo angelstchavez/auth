7f97587cefc839b06954d316d4baafe29275b27f
angelstchavez <angelstchavez@gmail.com>
2025-03-21 02:35:24 -0500

    feat: add metadata to some pages

--- components/landing/footer.tsx
+++ components/landing/footer.tsx
@@ -14,13 +14,13 @@ export default function Footer() {
         <nav className="flex items-center gap-4">
           <Link
             href="/terms-of-service"
-            className="text-sm text-neutral-400 hover:text-neutral-100"
+            className="text-sm hover:underline underline-offset-4"
           >
             Términos de Servicio
           </Link>
           <Link
             href="/privacy-policy"
-            className="text-sm text-neutral-400 hover:text-neutral-100"
+            className="text-sm hover:underline underline-offset-4"
           >
             Política de Privacidad
           </Link>
